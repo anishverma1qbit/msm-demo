f1c4ce9c03aad60f38b23c4a844305623d790983
anishverma1qbit <62626900+anishverma1qbit@users.noreply.github.com>
2021-10-26 13:49:38 -0700

    update repositories

--- demo-test.py
+++ demo-test.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from datetime import date
-import yfinance as yf
-from plotly import graph_objs as go
+# from plotly import graph_objs as go
 
 appTitle = "1QBit CME Market Sentiment Meter Demo"
 
@@ -18,4 +17,3 @@ dateSelectPrompt = "Please select the date range you want to examine."
 dateRange = st.sidebar.date_input(label = dateSelectPrompt, value = [date(2012,1,3), date(2021,10,22)])
 
 st.text(dateRange)
-
